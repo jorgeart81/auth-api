0e986c386b6420203ad0fd05c4cb74fb337b5209
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2025-02-05 18:57:03 -0500

    add securityStamp to token

--- AuthApi/Services/JWTService.cs
+++ AuthApi/Services/JWTService.cs
@@ -22,21 +22,23 @@ public class JWTService(IConfiguration configuration, UserManager<IdentityUser> 
     {
         if (jwtKey is null) throw new Exception("JWTKey is null. Please provide a valid JWT key.");
 
-        if (user.Email is null) throw new Exception("User is null.");
+        if (user.Email is null || user.SecurityStamp is null) throw new Exception("User is null.");
 
         var claims = new List<Claim>()
         {
-            new Claim("email", user.Email)
+            new Claim("email", user.Email),
+            new Claim("securityStamp", user.SecurityStamp)
         };
 
         var claimsDb = await userManager.GetClaimsAsync(user);
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var notBefore = DateTime.UtcNow;
         var expiration = DateTime.UtcNow.AddMinutes(minutes);
 
         claims.AddRange(claimsDb);
 
-        var securityToken = new JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expiration, signingCredentials: creds);
+        var securityToken = new JwtSecurityToken(issuer: null, audience: null, claims: claims, notBefore: notBefore, expires: expiration, signingCredentials: creds);
 
         return (securityToken, expiration);
     }
