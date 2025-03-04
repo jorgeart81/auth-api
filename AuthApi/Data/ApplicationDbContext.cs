754de15f97a0a8679600492a1a1816a3a2e3ed82
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-03-03 19:36:22 -0500

    add migration

--- AuthApi/Data/ApplicationDbContext.cs
+++ AuthApi/Data/ApplicationDbContext.cs
@@ -11,4 +11,24 @@ public class ApplicationDbContext : IdentityDbContext
     }
 
     public DbSet<Student> Students { get; set; }
+    public DbSet<Quiz> Quizzes { get; set; }
+    public DbSet<License> Licenses { get; set; }
+    public DbSet<Exam> Exams { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Quiz>()
+            .HasIndex(q => q.Title)
+            .IsUnique();
+
+        builder.Entity<License>()
+            .HasIndex(q => q.LicenseName)
+            .IsUnique();
+
+        builder.Entity<Exam>()
+            .HasIndex(q => q.ExamName)
+            .IsUnique();
+    }
 }
---
+++ AuthApi/Migrations/20250304003246_ExamsLicensesQuizzes.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AuthApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExamsLicensesQuizzes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Exams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ExamName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Licenses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LicenseName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ExamId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Licenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Licenses_Exams_ExamId",
+                        column: x => x.ExamId,
+                        principalTable: "Exams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quizzes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    PublishedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LicenseId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quizzes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Quizzes_Licenses_LicenseId",
+                        column: x => x.LicenseId,
+                        principalTable: "Licenses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exams_ExamName",
+                table: "Exams",
+                column: "ExamName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Licenses_ExamId",
+                table: "Licenses",
+                column: "ExamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Licenses_LicenseName",
+                table: "Licenses",
+                column: "LicenseName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quizzes_LicenseId",
+                table: "Quizzes",
+                column: "LicenseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quizzes_Title",
+                table: "Quizzes",
+                column: "Title",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quizzes");
+
+            migrationBuilder.DropTable(
+                name: "Licenses");
+
+            migrationBuilder.DropTable(
+                name: "Exams");
+        }
+    }
+}
