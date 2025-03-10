fc01ab210f12f46588d61396cc2fe835b755ab8b
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2025-03-10 16:28:24 -0500

    add DefaultAdmin

---
+++ AuthApi/Data/DefaultAdmin.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace AuthApi.Data;
+public class DefaultAdmin
+{
+    public required string DefaultUsername { get; set; }
+    public required string DefaultPassword { get; set; }
+}
